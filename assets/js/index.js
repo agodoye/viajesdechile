869cbfc544773d9ef5e339858c1f76449c208a30
Andres Godoy <andresgodoye@gmail.com>
2022-11-18 18:06:14 -0300

    animaciones y ajustes aplicados

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,46 @@
-$(document).ready(function () {
-    var scroll_pos = 0;
-    $(".navbar").scroll(function () {
-        scroll_pos = $(this).scrollTop();
-        if (scroll_pos > 50) {
-            $(".navbar").css('background-color', 'black');
-        } else {
-            $(".navbar").css('background-color', 'black');
-        }          
+$(document).ready(function(){
+    // TOOLTIPS
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    // CAMBIO COLOR NAV SCROLL
+    $(function () {
+        $(document).scroll(function () {
+          var $nav = $(".navbar");
+          $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+      });
+
+    // CAMBIO DE COLOR
+    $(".icono").on( "mouseover", function() {
+          $(this).css({
+            "color":"#373A3C",
+            "background-color":"lightyellow",
+          });
+        });
+        
+    $(".icono").on( "mouseout", function() {
+          $(this).css({
+            "color":"white",
+            "background-color":"rgb(13,202,240)",
+          });
+        });
+        
+    // OCULTAR INFORMACION RECETAS RELACIONADAS
+    $( "#icono1" ).click(function() {
+        $( "#ocultar1" ).fadeToggle(100);
     });
+
+    $( "#icono2" ).click(function() {
+        $( "#ocultar2" ).fadeToggle(100);
+    });
+
+    $( "#icono3" ).click(function() {
+        $( "#ocultar3" ).fadeToggle(100);
+    });
+
+    // ALERTA BOTON ENVIAR
+    $( "#enviar" ).click(function() {
+        alert( "Mensaje enviado correctamente." );
+      });
 });
